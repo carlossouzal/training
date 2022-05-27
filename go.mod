480d2cfa1b2ee413c2abfe7f8848a45f1e1df1c6
Carlos De Souza Lima <c101136@unimedpg.local>
2022-05-27 14:49:30 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/carlossouzal/training.git
+
+go 1.18
---
+++ judge/beginner/1001/extremamenteBasico.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A,
+		B int
+
+	fmt.Scan(&A)
+	fmt.Scan(&B)
+
+	x := A + B
+	fmt.Println("X = ", x)
+}
